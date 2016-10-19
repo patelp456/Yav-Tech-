b64962b2b178e40287602f6093a5a9a56cbfae2b
patelp456 <patelpawan456@gmail.com>
2016-10-19 08:58:10 +0530

    plotting graph for mean, high and low values

--- assign1/mean_deviation.js
+++ assign1/mean_deviation.js
@@ -11,75 +11,33 @@ function stdDeviation (data) {
 };
 
 // function for embedding mean and std in the chart
-function generateChart (high,low,meanHigh,meanLow,stdHigh,stdLow) {
+function generateChart (high,low,mean) {
 	var chart = c3.generate({
 		bindto: document.getElementById('dvImportSegments'),
 	    data: {
-	      	xs: {
-	      		'high': 'x1',
-	      		'low': 'x1',
-	            'meanHigh': 'x2',
-	            'stdHigh1': 'x2',
-	            'stdHigh2': 'x2',
-	            'meanLow': 'x3',
-	            'stdLow1': 'x3',
-	            'stdLow2': 'x3',
-	        },
 		    columns: [
-		    	['x1',1,2,3,4,5,6,7,8,9,10,11,12],
-		    	['x2',6],
-		    	['x3',7],
 	         	['high', high[0],high[1],high[2],high[3],high[4],high[5],high[6],high[7],high[8],high[9],high[10],high[11]],
 	         	['low',low[0],low[1],low[2],low[3],low[4],low[5],low[6],low[7],low[8],low[9],low[10],low[11]],
-	         	['meanHigh', meanHigh],
-	         	['meanLow', meanLow],
-	         	['stdHigh1', meanHigh + stdHigh],
-	         	['stdLow1', meanLow + stdLow],
-	         	['stdHigh2', meanHigh - stdHigh],
-	         	['stdLow2', meanLow - stdLow],
+	         	['mean', mean[0],mean[1],mean[2],mean[3],mean[4],mean[5],mean[6],mean[7],mean[8],mean[9],mean[10],mean[11]],
 		      ],
-		    type: 'bar',
-		    types: {
-		         high: 'line',
-		         low: 'line',
-		         stdHigh1: 'scatter',
-		         stdHigh2: 'scatter',
-		         stdLow1: 'scatter',
-		         stdLow2: 'scatter',
-		    },
-		    groups: [
-		         ['high','low'],
-		         ['meanHIgh', 'stdHigh1', 'stdHigh2'],
-		         ['meanLow','stdLow1','stdLow2']
-		    ]
-	    },
-	    bar: 
-	    {
-	        width: {
-	            ratio: 0.5 // this makes bar width 50% of length between ticks
-	        }
-	        // or
-	        //width: 100 // this makes bar width 100px
+		    type: 'line'
 	    }
 	});
 };
 
 function ChartHandler (data) {
 	
-	var high = new Array();
-	var low = new Array();
+	var high = new Array(); // array of high values
+	var low = new Array();  // array of low values
+	var mean = new Array(); // array of mean values
 	var i;
 	for (i = 1; i < data.length; i++) { 
 	    high[i-1] = data[i][0];
 	    low[i-1] = data[i][1];
+	    mean[i-1] = math.mean(high[i-1],low[i-1]);
 	}
 
-	var meanHigh = mean(high);
-	var meanLow = mean(low);
-	var stdHigh = stdDeviation(high);
-	var stdLow = stdDeviation(low);
-
-	generateChart(high,low,meanHigh,meanLow,stdHigh,stdLow);
+	generateChart(high,low,mean);
 };
 
 $(document).ready(function() {
@@ -122,4 +80,3 @@ function upload(evt) {
         }
     }
 });
-
